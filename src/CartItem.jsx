bbc783c06e054b32a65c7556fd09977088e38ddf
paolapetitti123 <paolaf0980@gmail.com>
2024-10-09 16:58:17 -0400

    fixed clicking home icon

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -16,6 +16,10 @@ const CartItem = ({ onContinueShopping }) => {
     alert('Functionality to be added for future reference');
   };
 
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
+
   const handleIncrement = (item) => {
     dispatch(updateQuantity({...item, quantity: item.quantity + 1}));
   };
@@ -66,7 +70,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
